b3408e4fb737f8b075f7db616bac4b4d3aa1d023
kunal132k <kunal132k@gmail.com>
2021-07-19 01:00:12 +0530

    Final Commit

--- server/index.js
+++ server/index.js
@@ -5,22 +5,19 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Hello to memories API');
-})
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://kunal132k:24196789@cluster0.cec2t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
